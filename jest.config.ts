a2be0cacc8a41605109debb8e97828dc4c5eb39e
stewie <stewiemayer@hotmail.com>
2025-08-13 10:34:36 -0600

    chore: se instala y configura rtl

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,7 @@ const config: Config = {
   collectCoverage: true,
   coverageDirectory: "coverage",
   testEnvironment: "jsdom",
+  setupFilesAfterEnv:["<rootDir>/src/setupTest.ts"]
 };
 
 export default config;
